7a72086cffc5a3b84aa991895b34ee2a345aeaa5
jessicahoward <jessicahoward1031@gmail.com>
2019-11-19 15:20:56 -0500

    display sign-up form

--- trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/HelloTestController.java
+++ trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/HelloTestController.java
@@ -1,15 +1,35 @@
 package org.launchcode.trailerqueen.controllers;
 
+import org.launchcode.trailerqueen.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.launchcode.trailerqueen.models.data.UserDao;
+
+import javax.validation.Valid;
 
 @Controller
 public class HelloTestController {
 
+    @Autowired
+    private UserDao userDao;
+
     @RequestMapping(value = "hello")
     @ResponseBody
-    public String index() {
+    public String hello() {
         return "Hello Cruel World";
     }
+
+    
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String displayCreateUserForm(Model model) {
+
+        model.addAttribute("title", "Create Account");
+        model.addAttribute(new User());
+        return "user/index";
+    }
+
+
 }
---
+++ trailer-queen/src/main/java/org/launchcode/trailerqueen/models/User.java
@@ -0,0 +1,52 @@
+package org.launchcode.trailerqueen.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=5, max=20, message = "password issue")
+    private String username;
+
+    @NotNull
+    @Size(min=8, max=20, message = "password issue")
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
