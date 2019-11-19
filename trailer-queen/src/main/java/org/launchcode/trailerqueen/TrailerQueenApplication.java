6bdca80a249fcb8bc85252338b3d616f5087164a
jessicahoward <jessicahoward1031@gmail.com>
2019-11-19 12:02:19 -0500

    springboot skeleon

---
+++ trailer-queen/src/main/java/org/launchcode/trailerqueen/TrailerQueenApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.trailerqueen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailerQueenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailerQueenApplication.class, args);
+	}
+
+}
---
+++ trailer-queen/src/test/java/org/launchcode/trailerqueen/TrailerQueenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.trailerqueen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailerQueenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
