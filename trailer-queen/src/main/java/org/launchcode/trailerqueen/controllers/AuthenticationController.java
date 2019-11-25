c112006b6336da8dc63037672d7a7833702dd408
jessicahoward <jessicahoward1031@gmail.com>
2019-11-25 12:36:20 -0500

    separate homepage functions to designated controller

--- trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/AuthenticationController.java
+++ trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/AuthenticationController.java
@@ -3,9 +3,6 @@ package org.launchcode.trailerqueen.controllers;
 import org.launchcode.trailerqueen.models.User;
 import org.launchcode.trailerqueen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -13,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 
@@ -37,13 +32,6 @@ public class AuthenticationController {
          User user = new User();
          modelAndView.addObject("user", user);
         modelAndView.setViewName("register"); // resources/template/register.html
-        return modelAndView;
-    }
-
-    @RequestMapping(value = "home", method = RequestMethod.GET)
-    public ModelAndView home() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home"); // resources/template/home.html
         return modelAndView;
     }
 
---
+++ trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package org.launchcode.trailerqueen.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "home", method = RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home"); // resources/template/home.html
+        return modelAndView;
+    }
+}
