9949f6831f5d32a354b5ba9256125c3b241731ca
jessicahoward <jessicahoward1031@gmail.com>
2019-11-26 09:29:35 -0500

    hitting dbraap, cookie issue when returning associated results

--- trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/HomeController.java
+++ trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/HomeController.java
@@ -7,15 +7,22 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.params.CookiePolicy;
+import org.apache.http.client.params.HttpClientParams;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 @Controller
 public class HomeController {
@@ -27,7 +34,7 @@ public class HomeController {
 
     @RequestMapping(value = "home", method = RequestMethod.POST)
     @ResponseBody
-    public String processHome(Model model, @RequestParam String zip) throws InterruptedException, ApiException, IOException {
+    public String processHome(Model model, @RequestParam String zip) throws InterruptedException, ApiException, IOException, UnirestException {
 
         String locationSearchParam = zip;
 
@@ -37,7 +44,34 @@ public class HomeController {
         GeocodingResult[] results = GeocodingApi.geocode(context, zip).await();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        return gson.toJson(results[0].geometry.location);
+        String latDegree = gson.toJson(results[0].geometry.location.lat);
+        String longDegree = gson.toJson(results[0].geometry.location.lng);
+//        TODO 1: use lat/long and any additional RequestParams to access dbraap API
+//        TODO 2: return dbraap results
+
+        String host = "https://brappdbv2.p.rapidapi.com/Parks";
+        String charset = "UTF-8";
+        String xRapidApiHost = "brappdbv2.p.rapidapi.com";
+        String xRapidApiKey = "daJQtrmBjimshXqYFh1HBrEKaQZop1ERXVXjsnp8CgR0U1Me4u";
+        String authorization = "DEj1j3JKBjzFIjN2rdAdTec8c40tNjuozluyfOQOjBw";
+
+        String s = "Hardrock Cycle Park";
+        String query = String.format("s=%s", URLEncoder.encode(s, charset));
+
+//        ####################################################
+//        ####################################################
+
+        HttpResponse<JsonNode> response = Unirest.get(host + "?" + query)
+                .header("x-rapidapi-host", xRapidApiHost)
+                .header("x-rapidapi-key", xRapidApiKey)
+                .header("authorization", authorization)
+                .asJson();
+
+
+        System.out.println(response.getStatus());
+        System.out.println(response.getHeaders().get("Content-Type"));
+        return latDegree + " " + longDegree;
+
 
     }
 }
