ffcc1926a5e284c2257dac0da52b2a5aa0ac8688
jessicahoward <jessicahoward1031@gmail.com>
2019-12-02 10:17:22 -0500

    start on results display

--- trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/HomeController.java
+++ trailer-queen/src/main/java/org/launchcode/trailerqueen/controllers/HomeController.java
@@ -1,6 +1,5 @@
 package org.launchcode.trailerqueen.controllers;
 
-
 import com.google.gson.*;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
@@ -14,6 +13,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.launchcode.trailerqueen.models.Park;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,18 +22,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "home", method = RequestMethod.GET)
     public String displayHome(Model model) {
+
+//        TODO: Pass favorites to home view
         return "home";
     }
 
     @RequestMapping(value = "home", method = RequestMethod.POST)
-    @ResponseBody
     public String processHome(Model model, @RequestParam String zip,
                               @RequestParam String dist,
                               @RequestParam String vehicle,
@@ -46,9 +48,9 @@ public class HomeController {
         GeocodingResult[] results = GeocodingApi.geocode(context, zip).await();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        String latDegree = gson.toJson(results[0].geometry.location.lat);
-        String longDegree = gson.toJson(results[0].geometry.location.lng);
-//        TODO 4: HIDE YOUR API KEYS DUMMY
+        String googLat = gson.toJson(results[0].geometry.location.lat);
+        String googLng = gson.toJson(results[0].geometry.location.lng);
+//        TODO : HIDE YOUR API KEYS DUMMY!!
 
         String host = "https://brappdbv2.p.rapidapi.com/Parks";
         String xRapidApiHost = "brappdbv2.p.rapidapi.com";
@@ -60,7 +62,7 @@ public class HomeController {
         String allowed = "allowedvehicles=" + vehicle;
         String expLevel = "ExperienceLevel=" + experience;
         String obstacles = "TerrainHas" + terrain;
-        String query = allowed + "&" + expLevel + "&" + obstacles + "&" + alwaysOpen + "&" + "lat=" + latDegree + "&" + "lng=" + longDegree + "&" + distance;
+        String query = allowed + "&" + expLevel + "&" + obstacles + "&" + alwaysOpen + "&" + "lat=" + googLat + "&" + "lng=" + googLng + "&" + distance;
 
         HttpResponse<JsonNode> response = Unirest.get(host + "?" + query)
                 .header("x-rapidapi-host", xRapidApiHost)
@@ -74,16 +76,45 @@ public class HomeController {
 
         JSONObject obj = new JSONObject(response.getBody().toString());
 
+        String name;
+        String desc;
+        String parkLat;
+        String parkLng;
+        JSONObject hazards;
+        JSONObject type;
+        JSONObject level;
+
+        ArrayList<Park> resultsList = new ArrayList<>();
         JSONArray stuff = (JSONArray) obj.getJSONArray("data");
         for (int i = 0; i < stuff.length(); i++) {
-            String name = stuff.getJSONObject(i).getString("Name");
-            String desc = stuff.getJSONObject(i).getString("Desc");
-            System.out.println("********************" + name);
-            System.out.println("********************" + desc);
+            name = stuff.getJSONObject(i).getString("Name");
+            desc = stuff.getJSONObject(i).getString("Desc");
+            parkLat = stuff.getJSONObject(i).getString("Lat");
+            parkLng = stuff.getJSONObject(i).getString("Lng");
+//            TODO: What to do with these objects?
+            hazards = stuff.getJSONObject(i).getJSONObject("terrain");
+            type = stuff.getJSONObject(i).getJSONObject("permittedVehicles");
+            level = stuff.getJSONObject(i).getJSONObject("experienceLevel");
 
+            Park aPark = new Park(name, desc, parkLat, parkLng, hazards, type, level);
+            resultsList.add(aPark);
+//            System.out.println("********NAME********" + name);
+//            System.out.println("********LAT*********" + parkLat);
+//            System.out.println("********LNG*********" + parkLng);
+//            System.out.println("********DESC********" + desc);
+//            System.out.println("******TERRAIN*******" + hazards);
+//            System.out.println("******ALLOWED*******" + type);
+//            System.out.println("*********EXP********" + level);
         }
 
-        return prettyJsonString;
+        for (Park park: resultsList) {
+            System.out.println("############ " + park.getAllowedVehicles());
+        }
+
+        model.addAttribute("parks", resultsList);
+        return "results";
+
+//        return prettyJsonString;
 
     }
 }
---
+++ trailer-queen/src/main/java/org/launchcode/trailerqueen/models/Park.java
@@ -0,0 +1,106 @@
+package org.launchcode.trailerqueen.models;
+
+import org.json.JSONObject;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Park {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private String latitude;
+
+    private String longitude;
+
+    private JSONObject listedTerrain;
+
+    private JSONObject allowedVehicles;
+
+    private JSONObject experienceLevel;
+
+    public Park() {
+    }
+
+    public Park(String name, String description, String latitude, String longitude, JSONObject listedTerrain, JSONObject allowedVehicles, JSONObject experienceLevel) {
+        this.name = name;
+        this.description = description;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.listedTerrain = listedTerrain;
+        this.allowedVehicles = allowedVehicles;
+        this.experienceLevel = experienceLevel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public JSONObject getListedTerrain() {
+        return listedTerrain;
+    }
+
+    public void setListedTerrain(JSONObject listedTerrain) {
+        this.listedTerrain = listedTerrain;
+    }
+
+    public JSONObject getAllowedVehicles() {
+        return allowedVehicles;
+    }
+
+    public void setAllowedVehicles(JSONObject allowedVehicles) {
+        this.allowedVehicles = allowedVehicles;
+    }
+
+    public JSONObject getExperienceLevel() {
+        return experienceLevel;
+    }
+
+    public void setExperienceLevel(JSONObject experienceLevel) {
+        this.experienceLevel = experienceLevel;
+    }
+}
